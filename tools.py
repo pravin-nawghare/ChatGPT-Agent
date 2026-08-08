beb5688473c18ebe6e377dd5d760af0fdf9dba5d
pravin-nawghare <nawgharepraveen@gmail.com>
2026-08-08 12:16:17 +0530

    web search and custom calculator tool added

--- tools.py
+++ tools.py
@@ -1,1 +1,30 @@
 # Tools for agent
+import math
+
+from langchain_core.tools import tool
+from langchain_tavily import TavilySearch
+
+# create tools
+
+web_search = TavilySearch( # no tool decorator because Tavily is inherientaly a tool
+    max_results = 5,
+    topic = "general",
+    search_depth = "advanced"
+)
+
+def calculator(expression: str) -> str:
+    try:
+        allowed = {
+            "math": math,
+            "abs": abs,
+            "round": round,
+            "min": min,
+            "max": max,
+            "sum" : sum
+        }
+
+        result = eval(expression, {"__builtins__": {}}, allowed)
+        return str(result)
+
+    except Exception as e:
+        return f"Calculation error: {str(e)}"
